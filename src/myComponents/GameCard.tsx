1c674b6717fd750cb6f2081cce7b3490ae90adbd
Ebube Nwachukwu <nebube705@gmail.com>
2025-11-10 19:38:05 -0500

    Displaying critic score

--- src/myComponents/GameCard.tsx
+++ src/myComponents/GameCard.tsx
@@ -1,8 +1,9 @@
 import { useColorMode, useColorModeValue } from "@/components/ui/color-mode";
 import type { Game } from "@/hooks/useGames";
 import { Card, CardBody } from "@chakra-ui/card";
-import { Heading, Image, Text } from "@chakra-ui/react";
+import { Heading, HStack, Image, Text } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
 
 interface Props {
   game: Game;
@@ -26,9 +27,12 @@ function GameCard({ game }: Props) {
         <Image src={game.background_image} />
         <CardBody>
           <Heading fontSize="2xl">{game.name}</Heading>
-          <PlatformIconList
-            platforms={game.parent_platforms.map((p) => p.platform)}
-          />
+          <HStack justifyContent="space-between">
+            <PlatformIconList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+            <CriticScore score={game.metacritic} />
+          </HStack>
         </CardBody>
       </Card>
     </>
